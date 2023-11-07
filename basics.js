18ebc56cb2bea1ca70ea71f6770d6e9c8857c8fb
Patra Kirsani <patrakirsani.234ca035@nitk.edu.in>
2023-11-07 15:27:03 +0530

    First commit

---
+++ basics.js
@@ -0,0 +1,18 @@
+const accountId=24423
+let accountEmail="patrakirsani@gmail.com"
+var accountPassword="Jaipur"
+let accountCity="jaipur"
+let Patra
+//accountId=3
+
+console.log(accountId);
+console.log(accountCity);
+
+accountEmail='Nothing'
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,Patra])
---
+++ dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" //treat all JS code as newer 
+
+// alert(3+34)   we are using nodejs, not brower
+
+console.log(34+
+  
+  4) // code readability should be high
+
+console.log("Patra ");
+
+let name ="Kirsani"
+let age=23;
+let isLoggedIn=false
+
+// number => 2 to power 53
+//bigint
+//string=>""
+//boolean
+//null=>standalone value
+//undefined=> 
+//symbol => 
+
+//object
+
+console.log(typeof(23));
+console.log(null);
+console.log(typeof null);
